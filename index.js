555b1f3963bb3fab0dd7fd333e59ea183bc0afdf
kyhyeok <kyhyeok@gmail.com>
2020-01-03 09:38:39 +0900

    middlewares

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
 const PORT = 4000;
@@ -6,9 +10,15 @@ const PORT = 4000;
 const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
 
-const handleHome = (req, res) => res.send("Hello from my ass");
+const handleHome = (req, res) => res.send("Hello from my ass!!");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
