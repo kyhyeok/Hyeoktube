63e88635225be4d726885614d89e7327cd658989
kyhyeok <kyhyeok@gmail.com>
2020-01-03 15:11:33 +0900

    MVC Part 2

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,11 @@
 import express from "express";
+import routes from "../routes";
 
 const userRouter = express.Router();
+
+userRouter.get(routes.users, (req, res) => res.send("Users"));
+userRouter.get(routes.userDetail, (req, res) => res.send("UserDetail"));
+userRouter.get(routes.editProfile, (req, res) => res.send("EditProfile"));
+userRouter.get(routes.changePassword, (req, res) => res.send("ChangePassword"));
 
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,11 @@
 import express from "express";
+import routes from "../routes";
 
 const videoRouter = express.Router();
+videoRouter.get(routes.videos, (req, res) => res.send("Videos"));
+videoRouter.get(routes.upload, (req, res) => res.send("Upload"));
+videoRouter.get(routes.videoDetail, (req, res) => res.send("VideoDetail"));
+videoRouter.get(routes.editVideo, (req, res) => res.send("EditVideo"));
+videoRouter.get(routes.deleteVideo, (req, res) => res.send("DeleteVideo"));
 
 export default videoRouter;
