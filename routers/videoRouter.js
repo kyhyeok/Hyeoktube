367ab80920cea1fda3e4ce2232dc93c44fffdd9e
kyhyeok <kyhyeok@gmail.com>
2020-01-03 17:56:26 +0900

    MVC Part 3

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,11 +1,18 @@
 import express from "express";
 import routes from "../routes";
+import {
+  videos,
+  upload,
+  videoDetail,
+  editVideo,
+  deleteVideo
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
-videoRouter.get(routes.videos, (req, res) => res.send("Videos"));
-videoRouter.get(routes.upload, (req, res) => res.send("Upload"));
-videoRouter.get(routes.videoDetail, (req, res) => res.send("VideoDetail"));
-videoRouter.get(routes.editVideo, (req, res) => res.send("EditVideo"));
-videoRouter.get(routes.deleteVideo, (req, res) => res.send("DeleteVideo"));
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
