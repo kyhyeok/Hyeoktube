52359b5a010d9d1f45282e6c2fe11935d8dd1798
kyhyeok <kyhyeok@gmail.com>
2020-01-06 18:41:46 +0900

    Home Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,7 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+import { videos } from "../db";
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
 
 export const search = (req, res) => {
   //   const searchingBy = req.query.term;
@@ -13,9 +16,6 @@ export const search = (req, res) => {
   } = req;
   res.render("search", { pageTitle: "Search", searchingBy });
 };
-
-export const videos = (req, res) =>
-  res.render("videos", { pageTitle: "Videos" });
 
 export const upload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -9,7 +9,6 @@ import {
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
-videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
