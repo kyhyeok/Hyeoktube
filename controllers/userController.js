e7a5396f7a351c4cb3ee4970c540226d1a3ac900
kyhyeok <kyhyeok@gmail.com>
2020-01-06 14:14:02 +0900

    Template Variables in Pug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,14 @@
 // export function join(req, res) {
 //   return res.send("join");
 // }
-export const join = (req, res) => res.render("Join");
-export const login = (req, res) => res.render("Login");
-export const logout = (req, res) => res.render("Logout");
-export const users = (req, res) => res.render("Users");
-export const userDetail = (req, res) => res.render("useRDetail");
-export const editProfile = (req, res) => res.render("editProfile");
-export const changePassword = (req, res) => res.render("changePassword");
+export const join = (req, res) => res.render("Join", { pageTitle: "Join" });
+export const login = (req, res) => res.render("Login", { pageTitle: "Login" });
+export const logout = (req, res) =>
+  res.render("Logout", { pageTitle: "Logout" });
+export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
+export const userDetail = (req, res) =>
+  res.render("useRDetail", { pageTitle: "User Detail" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "Change Password" });
