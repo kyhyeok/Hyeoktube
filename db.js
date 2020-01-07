55feeb327f9948fab0ec859d4a59c91cb11b16a8
kyhyeok <kyhyeok@gmail.com>
2020-01-07 18:44:05 +0900

    Configuring Dot Env

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-mongoose.connect("mongodb://localhost:27017/hyeok-tube", {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   userFindAndModify: false
 });
