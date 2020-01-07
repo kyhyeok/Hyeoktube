c7a302e48b6c8daf7d945c74f3bc8806bfeb270b
kyhyeok <kyhyeok@gmail.com>
2020-01-07 16:10:54 +0900

    More Controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,7 +22,12 @@ export const postJoin = (req, res) => {
   }
 };
 
-export const login = (req, res) => res.render("Login", { pageTitle: "Login" });
+export const getLogin = (req, res) => {
+  res.render("Login", { pageTitle: "Login" });
+};
+export const postLogin = (req, res) => {
+  res.redirect(routes.home);
+};
 export const logout = (req, res) =>
   res.render("Logout", { pageTitle: "Logout" });
 export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
