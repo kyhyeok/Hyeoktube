5e7becbb6e7d494bdac4226ae46cccb649981e5f
kyhyeok <kyhyeok@gmail.com>
2020-01-07 14:45:40 +0900

    Join Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,27 @@
+import routes from "../routes";
 // export function join(req, res) {
 //   return res.send("join");
 // }
-export const join = (req, res) => res.render("Join", { pageTitle: "Join" });
+export const getJoin = (req, res) => {
+  res.render("Join", { pageTitle: "Join" });
+};
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if (password !== password2 || !name || !email) {
+    res.status(400);
+    res.render("Join", { pageTitle: "Join" });
+  } else if (!name) {
+    res.status(400);
+    res.render("Join", { pageTitle: "Join" });
+  } else {
+    // To Do: register User
+    // To Do: log user in
+    res.redirect(routes.home);
+  }
+};
+
 export const login = (req, res) => res.render("Login", { pageTitle: "Login" });
 export const logout = (req, res) =>
   res.render("Logout", { pageTitle: "Logout" });
